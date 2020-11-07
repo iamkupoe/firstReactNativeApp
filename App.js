2b47f1173875f8462e9d9b94c7f590da8b163966
Samuel Kupoe <samuelkupoe1@gmail.com>
2020-11-07 15:25:47 +0000

    My first React Native App

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import profile from './assets/profile.jpg';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={profile} style={styles.image}/>
+
+      <View style={styles.detailContainer}>
+       <Text style={styles.label}>Name</Text>
+       <Text style={[styles.label, styles.info]}>Tony Stark</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+       <Text style={styles.label}>Email</Text>
+       <Text style={[styles.label, styles.info]}>tonystark@gmail.com</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+       <Text style={styles.label}>Gender</Text>
+       <Text style={[styles.label, styles.info]}>Male</Text>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +29,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 10, 
+    marginTop:60
   },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom:30,
+    marginTop:50,
+    alignSelf:"center",
+    borderRadius:75
+  },
+  detailContainer: {
+    flexDirection:"row",
+    marginVertical:3
+  },
+  label:{
+    borderColor:"blue",
+    borderWidth:1,
+    fontSize: 20,
+    marginRight:3,
+    flex:2.5,
+    paddingHorizontal:5
+  },
+  info:{
+    flex:7.5,
+  }
 });
+
+export default App;
